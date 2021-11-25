814a1a1a02c4c843f61794ed3cf1b08b63e50d11
Mohamad <m.sanjarmoosavi@gmail.com>
2021-11-25 19:57:30 +0330

    Add And Config Pinia

---
+++ src/Repo/Main_repo.ts
@@ -0,0 +1,13 @@
+import { defineStore } from "pinia";
+
+
+export const useMainRepo = defineStore("mainRepo", {
+  state: () => {
+    return {
+      
+    };
+  },
+  actions: {
+   
+  },
+});
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './Routing'
 
 createApp(App)
 .use(router)
+.use(createPinia())
 .mount('#app')

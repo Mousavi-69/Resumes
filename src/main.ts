18311f1a4fe22ea7eb01d414c4655828cc3f5087
Mohamad <m.sanjarmoosavi@gmail.com>
2021-11-25 20:20:57 +0330

    Add And Config Font Awesome Icon

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './Routing'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret,faUsers} from '@fortawesome/free-solid-svg-icons';
+import { faTimesCircle,faCheckCircle} from '@fortawesome/free-regular-svg-icons';
+import { faInstagram,faTelegramPlane} from '@fortawesome/free-brands-svg-icons';
+library.add(faUserSecret,faUsers,faTimesCircle,faCheckCircle,faUserSecret,faInstagram,faTelegramPlane);
 
 createApp(App)
+.component('fa',FontAwesomeIcon)
 .use(router)
 .use(createPinia())
 .mount('#app')
--- src/pages/Main_page.vue
+++ src/pages/Main_page.vue
@@ -1,5 +1,11 @@
 <template>
 <div>
     Hello  ...
+    <fa :icon="['fas', 'users']"/>
+    <br/>
+        <fa :icon="['far', 'check-circle']"/>
+         <br/>
+        <fa :icon="['fab', 'telegram-plane']"/>
+    
 </div>
 </template>

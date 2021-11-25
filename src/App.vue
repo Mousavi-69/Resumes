8320c475e2ddc8374ccc3abcab0eb8809bddc8a5
Mohamad <m.sanjarmoosavi@gmail.com>
2021-11-25 19:40:01 +0330

    Add Routing And Config

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Main_page from './pages/Main_page.vue';
+
+</script>
+
+<template>
+<div class="container">
+  <img alt="Vue logo" src="./assets/logo.png" />
+      <router-view> </router-view>
+</div>
+
+
+</template>
+
+<style>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './Routing'
+
+createApp(App)
+.use(router)
+.mount('#app')

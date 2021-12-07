37cf8dcea86cb79d1adec6acfebc05fe4f22e406
Mohamad <m.sanjarmoosavi@gmail.com>
2021-12-07 23:46:11 +0330

    add MyPortfolio in Main page

--- src/pages/Main_page.vue
+++ src/pages/Main_page.vue
@@ -1,29 +1,30 @@
 <template>
-<div>
+  <div>
     <headerVue class="header"></headerVue>
     <IntroductionVue class="IntroductionVue"></IntroductionVue>
     <AboutMe></AboutMe>
     <MyService></MyService>
     <SectionResume></SectionResume>
-  
-        <!-- <fa :icon="['fab', 'telegram-plane']"/> -->
-    
-</div>
+    <MyPortfolio></MyPortfolio>
+
+    <!-- <fa :icon="['fab', 'telegram-plane']"/> -->
+  </div>
 </template>
 <script setup lang="ts">
-import headerVue from '@/components/Header.vue';
-import IntroductionVue from '@/components/Introduction.vue';
-import AboutMe from '@/components/AboutMe.vue';
-import MyService from '@/components/MyService.vue';
-import SectionResume from '@/components/SectionResume.vue';
+import headerVue from "@/components/Header.vue";
+import IntroductionVue from "@/components/Introduction.vue";
+import AboutMe from "@/components/AboutMe.vue";
+import MyService from "@/components/MyService.vue";
+import SectionResume from "@/components/SectionResume.vue";
+import MyPortfolio from "@/components/MyPortfolio.vue";
 </script>
 <style scoped lang="scss">
-.header{
-    position: fixed;
-    width: 100%;
-    z-index: 50;
+.header {
+  position: fixed;
+  width: 100%;
+  z-index: 50;
 }
-.IntroductionVue{
-    padding-top: 125px;
+.IntroductionVue {
+  padding-top: 125px;
 }
-    </style>
+</style>

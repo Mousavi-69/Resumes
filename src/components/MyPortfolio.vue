c40925198b52381c009423e6104186510282121b
Mohamad <m.sanjarmoosavi@gmail.com>
2021-12-07 23:47:49 +0330

    My Portfolio level 1

--- src/components/MyPortfolio.vue
+++ src/components/MyPortfolio.vue
@@ -1,9 +1,57 @@
 <script setup lang="ts">
+import PortfolioComponent from "./PortfolioComponent.vue";
+import buttonVue from "./Button.vue";
 </script>
 <template>
-<div>
-    Portfolio
-</div>
+  <div class="container">
+    <div class="title">
+      <h2>خدمات من</h2>
+      <h4>
+        من کارهای زیبا را طراحی و کد می کنم و کاری را که می کنم دوست دارم.
+      </h4>
+    </div>
+    <section class="portfolio">
+      <PortfolioComponent></PortfolioComponent>
+      <PortfolioComponent></PortfolioComponent>
+      <PortfolioComponent></PortfolioComponent
+      ><PortfolioComponent></PortfolioComponent>
+      <PortfolioComponent></PortfolioComponent>
+      <PortfolioComponent></PortfolioComponent>
+    </section>
+    <footer class="footer">
+      <buttonVue
+        class="btn"
+        :width-btn="185"
+        :height-btn="52"
+        :text="'مشاهده سایر '"
+        :id="'#'"
+        :bg-color="'#754ef9'"
+        :text-color="'white'"
+        :hover-bg-color="'white'"
+        :hover-text-color="'#754ef9'"
+        :border-color="'#754ef9'"
+      ></buttonVue>
+    </footer>
+  </div>
 </template>
-<style>
+<style scoped lang="scss">
+.container {
+  background-color: #fbfbfb;
+
+  .title {
+    padding: 50px 100px 30px 100px;
+  }
+}
+.portfolio {
+  padding: 0px 100px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 30px;
+  justify-content: center;
+}
+.footer {
+  display: flex;
+  justify-content: center;
+  margin-top: 35px;
+}
 </style>

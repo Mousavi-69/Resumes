ccea9c05b13c5bbc325dd6f8fdaaed02d0a7d04f
Mohamad <m.sanjarmoosavi@gmail.com>
2021-12-08 01:17:46 +0330

    Tutorials Level 1 OK

---
+++ src/components/Tutorials.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import TutorialsComponent from './TutorialsComponent.vue';</script>
+<template>
+  <div class="container">
+    <div class="title">
+      <h2>آموزش ها</h2>
+      <h4>
+        من کارهای زیبا را طراحی و کد می کنم و کاری را که می کنم دوست دارم.
+      </h4>
+    </div>
+    <div class="tutorials">
+<TutorialsComponent></TutorialsComponent>
+<TutorialsComponent></TutorialsComponent>
+<TutorialsComponent></TutorialsComponent>
+
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+.container {
+  background-color: #f7f7f7;
+  text-align: center;
+ .title{
+     padding-top: 30px;
+ }
+}
+.tutorials{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 35px;
+    padding: 50px 100px;
+}
+</style>

4bdfaeb8e5a2d20adbdb644bd6f2fdb8e6368be1
Mohamad <m.sanjarmoosavi@gmail.com>
2021-12-08 17:02:03 +0330

    Article level 2 OK

---
+++ src/components/MyArticles.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import ArticleComponent from "./ShapeComponent/ArticleComponent.vue";
+import buttonVue from "./Button.vue";
+</script>
+<template>
+  <div class="container">
+    <div class="title">
+      <h2>مقالات من</h2>
+      <h4>
+        من کارهای زیبا را طراحی و کد می کنم و کاری را که می کنم دوست دارم.
+      </h4>
+    </div>
+    <div class="article">
+      <ArticleComponent></ArticleComponent>
+      <ArticleComponent></ArticleComponent>
+      <ArticleComponent></ArticleComponent>
+    </div>
+    <div class="footer">
+      <buttonVue
+        class="btn"
+        :width-btn="185"
+        :height-btn="52"
+        :text="'مشاهده بیشتر '"
+        :id="'#'"
+        :bg-color="'#754ef9'"
+        :text-color="'white'"
+        :hover-bg-color="'white'"
+        :hover-text-color="'#754ef9'"
+        :border-color="'#754ef9'"
+      ></buttonVue>
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+.container {
+
+  background-color: white;
+  text-align: center;
+  box-sizing: border-box;
+  .title{
+    padding-top: 50px;
+    padding-bottom: 30px;
+  }
+}
+.article {
+  padding-left: 100px;
+  padding-right: 100px;
+  display: flex;
+  gap: 25px;
+  justify-content: center;
+  align-items: center;
+}
+.footer {
+  display: flex;
+  justify-content: center;
+  margin-top: 35px;
+  padding-bottom: 60px;
+}
+</style>
---
+++ src/components/ShapeComponent/ArticleComponent.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="container">
+    <div class="img">
+      <img src="@/assets/images/Articles/b-1.jpg" alt="Ecma Script Article" />
+      <div class="title">
+        <h2>اکما اسکریپت چیشت !؟</h2>
+        <h4>1400.9.17</h4>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+.container {
+  width: 350px;
+  height: 370px;
+  position: relative;
+  box-shadow: 0px 0px 15px 0px rgb(152 152 152 / 25%);
+  overflow: hidden;
+  cursor: pointer;
+
+  .img {
+    width: 100%;
+    vertical-align: middle;
+
+    &:hover {
+      img {
+        transform: scale(1.2);
+        transition: all 0.3s ease-out 0.15s;
+      }
+      .title {
+        background-color: #754ef9;
+        color: white;
+        transition: all 0.3s ease-out 0.15s;
+      }
+    }
+  }
+  .title {
+    position: absolute;
+    text-align: right;
+    padding: 2px 10px;
+    line-height: 10px;
+    bottom: 0;
+    right: 0;
+    width: 70%;
+    background-color: rgba(255, 255, 255, 0.8);
+  }
+}
+</style>

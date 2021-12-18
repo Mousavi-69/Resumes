15d6eac12632f515ee8ecf5e3f4667525cae2903
Mohamad <m.sanjarmoosavi@gmail.com>
2021-12-18 11:35:13 +0330

    Form hot fix

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,22 +1,32 @@
 <script setup lang="ts">
-import buttonVue from './Button.vue';</script>
+import buttonVue from "./Button.vue";
+</script>
 <template>
   <div class="container">
     <section class="form">
       <form>
         <input type="text" name="name" placeholder="نام و نام خانوادگی" />
         <input type="text" name="email" placeholder="پست الکترونیک" />
-        <input type="text" name="message" class="request" placeholder="متن درخواست" />
+        <textarea
+          class="request"
+          type="text"
+          name="message"
+          placeholder="متن درخواست"
+        />
       </form>
-<buttonVue class="btn" :text="'ارسال  پیام'" :id="id"
-          :width-btn="170"
-          :height-btn="55"
-          :bg-color="'#754ef9'"
-          :text-color="'white'"
-          :hover-bg-color="'white'"
-          :hover-text-color="'#754ef9'"
-          :border-color="'#754ef9'"
-        ></buttonVue>    </section>
+      <buttonVue
+        class="btn"
+        :text="'ارسال  پیام'"
+        :id="id"
+        :width-btn="170"
+        :height-btn="55"
+        :bg-color="'#754ef9'"
+        :text-color="'white'"
+        :hover-bg-color="'white'"
+        :hover-text-color="'#754ef9'"
+        :border-color="'#754ef9'"
+      ></buttonVue>
+    </section>
     <section class="map"></section>
   </div>
 </template>
@@ -30,13 +40,13 @@ import buttonVue from './Button.vue';</script>
     margin-bottom: 30px;
     border: 1px solid rgb(230, 230, 230);
     border-radius: 8px;
-    
     font-size: 14px;
     color: #8a8fa3;
     text-align: right;
-    &:focus{
-   outline: none !important;
-    border-color: #754ef9;
+    &:focus {
+      outline: none !important;
+      border-color: #754ef9;
+      color: black;
     }
   }
   .request {
@@ -44,6 +54,17 @@ import buttonVue from './Button.vue';</script>
     height: 140px;
     padding: 10px;
     margin-bottom: 30px;
+    border: 1px solid rgb(230, 230, 230);
+    border-radius: 8px;
+    font-size: 14px;
+    color: #8a8fa3;
+    text-align: right;
+    resize: none;
+    &:focus {
+      outline: none !important;
+      border-color: #754ef9;
+      color: black;
+    }
   }
 }
 </style>

21b162df16f62ebeae64547c3ff5ff5a060bd78a
Mohamad <m.sanjarmoosavi@gmail.com>
2021-12-22 19:15:03 +0330

    Add Icons

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,9 @@ import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUserSecret,faUsers,faBars,faPhone,faMapMarkerAlt,faInfo} from '@fortawesome/free-solid-svg-icons';
 import { faTimesCircle,faCheckCircle,faCalendarAlt,faEnvelope} from '@fortawesome/free-regular-svg-icons';
-import { faInstagram,faTelegramPlane,faGithub} from '@fortawesome/free-brands-svg-icons';
-library.add(faUserSecret,faUsers,faBars,faPhone,faMapMarkerAlt,faInfo,faTimesCircle,faCheckCircle,faCalendarAlt,faEnvelope,faUserSecret,faInstagram,faTelegramPlane,faGithub);
+import { faInstagram,faTelegramPlane,faGithub,faLinkedin,faWhatsapp} from '@fortawesome/free-brands-svg-icons';
+library.add(faUserSecret,faUsers,faBars,faPhone,faMapMarkerAlt,faInfo,faTimesCircle,faCheckCircle,
+    faCalendarAlt,faEnvelope,faUserSecret,faInstagram,faTelegramPlane,faGithub,faLinkedin,faWhatsapp);
 
 createApp(App)
 .component('fa',FontAwesomeIcon)

56e33d738fa0206170a65871b5ec387b2683e377
Mohamad <m.sanjarmoosavi@gmail.com>
2022-08-10 20:14:48 +0430

    Add props & change Css in TutorialComponent

--- src/components/ShapeComponent/TutorialsComponent.vue
+++ src/components/ShapeComponent/TutorialsComponent.vue
@@ -1,30 +1,43 @@
 <script setup lang="ts">
 import buttonVue from "../Button.vue";
+const props = defineProps<{
+  title: { type: string; default: "نام دوره" };
+  description: { type: string; default: "توضیحات دوره" };
+  price: { type: string; default: "رایگان" };
+  section1: { type: string; default: "دسترسی به کتاب ها" };
+  section2: { type: string; default: "پشتیبانی" };
+  section3: { type: string; default: "عضویت در باشگاه مغز" };
+  section4: { type: string; default: "هدیه:تم علوم شناختی"; required: false };
+}>();
 </script>
 <template>
   <div class="container">
     <div class="header">
-      <h3>Html/css</h3>
+      <h3 class="title">{{ props.title }}</h3>
     </div>
     <div class="section1">
-      <h3>مدیریت پروژه ساده برای تیم ها و مشاغل کوچک.</h3>
-      <h2>425000</h2>
+      <h4 class="description">{{ props.description }}</h4>
+      <div class="price">
+        <h4>{{ props.price }}</h4>
+        <h5 class="vahed">تومان</h5>
+      </div>
+       <hr />
     </div>
-    <hr />
+   
     <div class="section2">
-      <h3>وظایف نامحدود</h3>
-      <h3>پروژه های خصوصی</h3>
+      <h4>{{ props.section1 }}</h4>
+      <h4>{{ props.section2 }}</h4>
 
-      <h3>تیم های نامحدود</h3>
+      <h4>{{ props.section3 }}</h4>
 
-      <h3>API</h3>
+      <h4>{{ props.section4 }}</h4>
     </div>
     <div class="footer">
       <buttonVue
         class="btn"
         :width-btn="160"
         :height-btn="50"
-        :text="'افزودن به سبد خرید '"
+        :text="'ثبت نام دوره '"
         :id="'#'"
         :bg-color="'#754ef9'"
         :text-color="'white'"
@@ -37,34 +50,57 @@ import buttonVue from "../Button.vue";
 </template>
 <style scoped lang="scss">
 .container {
-  width: 330px;
-  height: 460px;
+  width: 300px;
+  height: 430px;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: column;
   text-align: center;
   cursor: pointer;
   overflow: hidden;
-  padding-bottom: 20px;
+  padding-bottom: 10px;
   border: 1px solid rgb(196, 194, 194);
   border-radius: 8px;
   transition: all 0.2s ease-out 0.2s;
-
 
   &:hover {
     transform: translateY(-7px);
     box-shadow: 0 1px 15px 5px rgb(216, 216, 216);
   }
+  
 }
 .header {
-  padding: 1px;
+  height: 50px;
   background-color: #754ef9;
+  .title {
+    color: white;
+  }
 }
 .section1 {
-  padding: 1px 20px;
+  height: 120px;
+  padding-right: 30px;
+  padding-left: 30px;
+  line-height: 20px;
+.description{
+  height: 60px;
+}  .price {
+      height: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .vahed {
+      padding-right: 5px;
+      color: black;
+    }
+  }
+}
+.section2{
+  height: 200px;
 }
 
 .footer {
+  height: 50px;
   display: flex;
   justify-content: center;
-  margin-top: 10px;
-  padding-bottom: 10px;
 }
 </style>
